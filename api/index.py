7efbd2e509ebafaabcc06a7600f3862f3733e9d1
MΛCHINΣ CODΣ <ask@ragu.cz>
2024-08-30 15:16:35 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,23 +1,10 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from pydantic import BaseModel
-from .scraper import process_url
+from http.server import BaseHTTPRequestHandler
 
-app = FastAPI()
+class handler(BaseHTTPRequestHandler):
 
-
-class UrlRequest(BaseModel):
-    url: str
-
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    try:
-        data = await websocket.receive_json()
-        url = data.get("url")
-        if url:
-            await process_url(url, websocket)
-        else:
-            await websocket.send_json({"error": "No URL provided"})
-    except WebSocketDisconnect:
-        print("WebSocket disconnected")
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
